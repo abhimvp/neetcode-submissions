03cbe14876540e0029100c53e2db1e3cb5a4ed2e
Abhishek Reddy Boddu <abhishekreddyboddu1@gmail.com>
2026-06-14 07:23:30 +0530

    Add: python-dynamic-typing - submission-0

---
+++ Python For Beginners/python-dynamic-typing/submission-0.py
@@ -0,0 +1,10 @@
+variable = 2 
+print(type(variable))
+variable = 2.5
+print(type(variable))
+variable = True
+print(type(variable))
+variable = "Shek"
+print(type(variable))
+variable = [2,3,4]
+print(type(variable))
